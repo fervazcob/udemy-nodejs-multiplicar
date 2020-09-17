d0b7048039af8ad1a0869ed17228fc2d17075bb0
Fernando Vazquez <fdovazqc@gmail.com>
2020-09-16 20:43:57 -0500

    Terminado

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const argv = require("./configs/yargs");
-const colors = require("colors");
+const colors = require("colors/safe");
 
 const { crearTabla, leerTabla } = require("./multiplicar/multiplicar");
 
@@ -8,13 +8,13 @@ let cmd = argv._[0];
 switch (cmd) {
   case "crear":
     crearTabla(argv.base, argv.limite)
-      .then((data) => console.log(data.green))
-      .catch((err) => console.error(err.red));
+      .then((data) => console.log(colors.green(data)))
+      .catch((err) => console.error(colors.red(err)));
     break;
   case "listar":
     leerTabla(argv.base, argv.limite)
-      .then((data) => console.log(data.green))
-      .catch((err) => console.error(err.red));
+      .then((data) => console.log(colors.green(data)))
+      .catch((err) => console.error(colors.red(err)));
     break;
 
   default:
